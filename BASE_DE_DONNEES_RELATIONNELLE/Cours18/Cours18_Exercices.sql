09d0eef6831bdb04a8dc3e1d81a5138bae290d06
pedjaceli <yannick.kananin@outlook.com>
2026-04-01 09:39:34 -0400

    Base de données

---
+++ BASE_DE_DONNEES_RELATIONNELLE/Cours18/Cours18_Exercices.sql
@@ -0,0 +1,34 @@
+-- -- -------------------------------------------------------------------------------------------------------------------------
+-- COURS18 - Exercices
+-- -------------------------------------------------------------------------------------------------------------------------
+/*
+CONTEXTE :
+Le Cégep veut une BD pour son centre sportif. On veut suivre les membres et les cours.
+On va aussi avoir une table pour faire le lien entre nos 2 autres tables.
+
+La liste des tables :
+- Membres
+- Cours
+- Inscriptions
+*/
+-- -------------------------------------------------------------------------------------------------------------------------
+-- Partie 0 : Faire une BD Cours18_Exercices
+-- -------------------------------------------------------------------------------------------------------------------------
+-- Partie 1 : Table Membres
+
+-- Les champs: il faut au minimum inclure le numéro du membre, son nom + prénom et son courriel.
+-- Le courriel doit être unique!
+-- -------------------------------------------------------------------------------------------------------------------------
+-- Partie 2 : Table Cours
+
+-- Les champs: le numéro du cours, le titre du cours et la durée en minutes du cours.
+-- -------------------------------------------------------------------------------------------------------------------------
+-- Partie 3 : Table Cours
+
+-- Les champs: le numéro du membre, le numéro du cours et un champ qui prend automatiquement la date et l'heure de l'inscription.
+-- Attention, un membre ne peut pas s'inscrire 2 fois au même cours!
+-- -------------------------------------------------------------------------------------------------------------------------
+-- Partie 4 : Ajout de données
+
+-- Ajoutez au moins 1 entrée dans chacune des tables pour tester!
+-- -------------------------------------------------------------------------------------------------------------------------
