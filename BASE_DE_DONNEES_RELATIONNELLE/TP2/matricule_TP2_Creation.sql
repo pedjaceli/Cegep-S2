f7adda3e42ca9b40dcf356ee576b3d01ac9fffd5
pedjaceli <yannick.kananin@outlook.com>
2026-04-22 08:13:25 -0400

    Ajout du TP2

---
+++ BASE_DE_DONNEES_RELATIONNELLE/TP2/matricule_TP2_Creation.sql
@@ -0,0 +1,110 @@
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+/*
+
+TP2 SQL 2026
+ÉCRIVEZ VOTRE MATRICULE SUR LA LIGNE SUIVANTE:
+
+
+*/
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Code de création de la table Heros
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Code de création de la table Classes
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Code de création de la table Competences
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Code de création de la table Quetes
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Code de création de la table Loots
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Code de création de la table Monstres
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Code de création de la table Configurations
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Code de création de la table ListeNoireTaverne
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Code de création des autres tables si nécessaire
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Code pour les contraintes supplémentaires ou si vous
+-- préférez les faire après la création des tables.
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Les ALTER TABLES si nécessaire
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Le INSERT de la table Heros
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Le INSERT de la table Classes
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Le INSERT de la table Competences
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Le INSERT de la table Quetes
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Le INSERT de la table Loots
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Le INSERT de la table Monstres
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Le INSERT de la table Configurations
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Le INSERT de la table ListeNoireTaverne
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- Les INSERT pour vos autres tables si nécessaire
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
